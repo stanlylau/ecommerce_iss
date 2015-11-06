cc3a32ea4d53bf6e900bdb80598367f7b0ce975d
Deadmetal85 <whyubotherme@gmail.com>
2015-11-06 15:23:16 +0800

    Refactored test case

--- test/iss/ShoppingCartTest.java
+++ test/iss/ShoppingCartTest.java
@@ -18,29 +18,27 @@ public class ShoppingCartTest extends TestCase{
 	}
 	
 	public void testBuyOneItem() {
-		Product product = new Product("apple","1001.1", 2);
-		OrderItem item = new OrderItem(product, 1);
-		
-		cart.add(item);
-		
+		OrderItem item = createOrderItem("apple","1001.1", 2, 1);
+		cart.add(item);	
 		assertEquals(2, cart.total(), 0.01);
 	}
 
 	public void testBuyTwoItemsOfSameProduct() {
-		Product product = new Product("apple","1001.1", 2);
-		OrderItem item = new OrderItem(product, 2);
-		
+		OrderItem item =  createOrderItem("apple","1001.1", 2, 2);
 		cart.add(item);
-		
 		assertEquals(3.4, cart.total(), 0.01);
 	}
 
 	public void testBuyTwoItemsOfSameProductX() {
-		Product product = new Product("apple","1001.1", 2);
-		OrderItem item = new OrderItem(product, 3);
-		
+		OrderItem item = createOrderItem("apple","1001.1", 2, 3);
 		cart.add(item);
-		
 		assertEquals(5.4, cart.total(), 0.01);
 	}
+	
+	private OrderItem createOrderItem(String name, String sku, double price, int quantity ) {
+		Product product = new Product(name, sku, price);
+		OrderItem item = new OrderItem(product, quantity);
+		return item;
+	}
+
 }
